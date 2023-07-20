f1b8b466a8a3ecbb2aab0182fe888652ca40b696
Taejun33312 <a01055384863@gmail.com>
2023-07-20 19:21:26 +0900

    application 수정

--- src/main/java/com/travel/controller/TravelInMapApplication.java
+++ src/main/java/com/travel/controller/TravelInMapApplication.java
@@ -2,8 +2,14 @@ package com.travel.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan("com.travel")
+@EntityScan(basePackages = "com.travel.entity")
+@EnableJpaRepositories(basePackages = "com.travel.persistence")
 public class TravelInMapApplication {
 
 	public static void main(String[] args) {
