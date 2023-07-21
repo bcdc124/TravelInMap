a45a43c4e937449adc3d5379a412484921be0d20
th <th@taehee>
2023-07-21 21:24:03 +0900

    0721 태희 - logger 추가

--- src/main/java/com/travel/persistence/TimMemberListImpl.java
+++
@@ -1,9 +0,0 @@
-package com.travel.persistence;
-
-import java.util.List;
-
-import com.travel.entity.TimMember;
-
-public interface TimMemberListImpl {
-	List<TimMember> getMemberList() throws Exception;
-}
--- src/main/java/com/travel/persistence/TimMemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.travel.persistence;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.travel.entity.TimMember;
-
-public interface TimMemberRepository extends JpaRepository<TimMember, String>{
-
-}
